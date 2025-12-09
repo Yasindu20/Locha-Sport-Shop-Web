cf411337be8a8f01af92e2a1705adef383ec0301
Lochana nimsara <136056300+laserXnext@users.noreply.github.com>
2025-12-09 11:27:46 +0530

    Slightly Updated Version

---
+++ frontend/Sport-Web/src/script/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../css/App.css'
+
+function App() {
+  return (
+    <>
+      <p>Updated vesrsion is this yasiya</p>
+    </>
+  )
+}
+
+export default App
