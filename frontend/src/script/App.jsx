a3f5e990da072419c02727059ed188f90cdce434
laserXnext <nimsaralochana60@gmail.com>
2025-12-28 15:45:37 +0530

    Contact Update

--- frontend/src/script/App.jsx
+++ frontend/src/script/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Home from "../pages/home";
 import About from "../pages/about";
+import Contact from "../pages/contact";
 
 import "../css/App.css";
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
